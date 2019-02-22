499bb13920bf380fd71f8f2cd1e381322abadb16
John Lougee <jlougee@gmail.com>
2019-02-22 16:17:36 -0500

    2-22 update

--- Lab_6/MongoEmployee/server.js
+++ Lab_6/MongoEmployee/server.js
@@ -21,44 +21,62 @@ mongoose.connect('mongodb://localhost:27017/Employee', {useNewUrlParser: true});
      console.log("We're connected!")
  });
 
-hbs.registerHelper('ptag',(num, messagePassed)=>{
-    var msg = '';
-    for(let i = 0; i < num; i++){
-        msg+=`<p>${messagePassed}</p><br>`;
-    }
-    //return msg;
-    //need this safeString function if you have html elements in your string
-    return new hbs.handlebars.SafeString(msg);
+hbs.registerHelper('genAll', ()=>{
+    var gen = ""
+    gen = "<table>"
+    gen += "<tbody>"
+
+    employee.find(function(err, data){
+        if (err) return console.error(err);                        
+        for(i = 0; i < data.length; i++){
+            gen += "<tr>"
+            gen += "<td>" + data[i].firstName + "</td>"
+            gen += "<td>" + data[i].lastName + "</td>"
+            gen += "<td>" + data[i].department+ "</td>"
+            gen += "<td>" + data[i].startDate+ "</td>"
+            gen += "<td>" + data[i].jobTitle + "</td>"
+            gen += "<td>" + data[i].salary + "</td>"
+            gen += "<td>" + "<a class='btn btn-primary' href='update.hbs'>Update</a>" + "</td>"
+            gen += "<td>" + "<a class='btn btn-primary' href='delete.hbs'>Delete</a>" + "</td>"
+            gen += "</tr>"
+        }
+        console.log("1:" + gen)        
+    })
+
+    gen += "</tbody>"
+    gen += "</table>"
+    return gen
 })
 
-app.get('/view', function (req, res){
-    employee.find(function(err, data){
-        if (err) return console.error(err);        
-        console.log(data);
-        var gen = "<table>"
-        gen += "<tbody>"
-        
-        for(var em in data){
-            gen += "<td>" + data.firstName + "</td>"
-        }
-        gen += "</tbody>"
-        gen += "</table>"
+hbs.registerHelper('oneEmp', (empId)=>{
+    db.employee.find(function(err, data, empId){
+        ObjectId(empId)
     })
-    res.render('view.hbs', {gen: gen});    
-});
+    // if(err) throw err;
+    // var query = {id: empId}
 
-app.post('/results', function(req, res, next) {    
-    var emp = new employee({firstName: req.body.firstName, lastName: req.body.lastName, department: req.body.ddDepartment, startDate: req.body.startDate, jobTitle: req.body.jobTitle, salary: req.body.salary});
-    emp.save(function(err, emp){
-        if(err) return console.error(err);
-        console.log("Employee " + emp + " Saved!");
-    })
-    res.render('view.hbs', {emp: emp});    
- })
+    // employee.find(query)
+})
 
- app.all('/index', (req, res)=>{
+// employee.query.byID = function(id) {
+//     return this.where({id: new RegExp(id, 'i')});
+// };
+
+
+
+app.all('/view', function(req, res, next) {    
+    res.render('view.hbs', {gen: res.gen});    
+})
+
+app.all('/index', (req, res)=>{
     res.render('form.hbs');
 })
+
+app.all('/test', (req, res)=>{
+    res.render('test.hbs');
+})
+
+
 app.all('/form', (req, res)=>{
     res.render('form.hbs');
 })
